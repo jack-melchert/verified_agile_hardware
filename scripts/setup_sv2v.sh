34cf581156ddcb435cb6628954de93a2a9ad95c4
jack-melchert <56329001+jack-melchert@users.noreply.github.com>
2023-07-20 14:18:40 -0700

    Update setup_sv2v.sh

--- scripts/setup_sv2v.sh
+++ scripts/setup_sv2v.sh
@@ -2,7 +2,10 @@ dir=$pwd
 sdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 mkdir -p $sdir/../deps
 cd $sdir/../deps
-curl -sSL https://get.haskellstack.org/ | sh
+if ! command -v stack &> /dev/null
+then
+    curl -sSL https://get.haskellstack.org/ | sh
+fi
 git clone https://github.com/zachjs/sv2v.git
 cd sv2v
 make
