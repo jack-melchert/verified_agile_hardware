1cbbfa5469c5a41cae80874d8b463ec46c8807b2
root <melchert@stanford.edu>
2024-02-02 16:00:00 -0800

    Trying to setup CI

--- scripts/setup_sv2v.sh
+++ scripts/setup_sv2v.sh
@@ -1,21 +1,26 @@
 pwd
 dirpwd=$pwd
+echo $dirpwd
+
 sdir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 mkdir -p $sdir/../deps
 cd $sdir/../deps
+echo $dirpwd
+
 if ! command -v stack &> /dev/null
 then
     curl -sSL https://get.haskellstack.org/ | sh
 fi
 git clone https://github.com/zachjs/sv2v.git
 cd sv2v
+echo $dirpwd
 make
 if grep -q docker /proc/1/cgroup; then 
     stack install --local-bin-path /bin/
 else
     stack install 
 fi
-
+echo $dirpwd
 cd $dirpwd
 pwd
 ls -al
